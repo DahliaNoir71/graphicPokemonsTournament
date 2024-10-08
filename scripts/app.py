92765059d2c0437902a71b568ebf3a2d2f1e1d46
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-08 10:55:28 +0200

    Refactor Pokémon management into a class
    Introduce the PokemonManager class to encapsulate Pokémon-related functionalities, improving code organization and readability. This refactoring centralizes methods for retrieving Pokémon data, calculating strength, and simulating battles within the new class.

--- scripts/app.py
+++ scripts/app.py
@@ -5,114 +5,103 @@ import requests
 app = Flask(__name__)
 
 BASE_URL_API = "https://pokeapi.co/api/v2/"
-URL_POKEMON_API_BASE = "%spokemon" % BASE_URL_API
-NB_PARTICIPANTS = 16
+URL_POKEMON_API_BASE = f"{BASE_URL_API}pokemon"
+PARTICIPANTS_COUNT = 2
 
 participants = []
+round_number = 1
 
-def get_random_pokemon_id(pokemons_count):
-    """
-    :param pokemons_count: Integer representing the total number of possible Pokémon.
-    :return: Integer representing a randomly selected Pokémon ID.
-    """
-    return random.randint(1, pokemons_count)
 
-def fetch_pokemon_data(pokemon_id):
-    """
-    :param pokemon_id: The ID of the Pokémon to retrieve data for.
-    :return: A dictionary containing the Pokémon data if the request is successful, or None if an error occurs.
-    """
-    try:
+class PokemonManager:
+    """Class to manage Pokémon data retrieval and battle simulation."""
+
+    @staticmethod
+    def get_random_pokemon_id(total_pokemons):
+        """
+        Return a randomly selected Pokémon ID.
+        """
+        return random.randint(1, total_pokemons)
+
+    @staticmethod
+    def fetch_pokemon_data(pokemon_id):
+        """
+        Retrieve data for the Pokémon with the given ID.
+        """
         url_pokemon = f"{URL_POKEMON_API_BASE}/{pokemon_id}"
-        response = requests.get(url_pokemon)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException:
-        print(f"Erreur lors de la récupération du Pokémon avec l'ID : {pokemon_id}")
-        return None
+        try:
+            response = requests.get(url_pokemon)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException:
+            print(f"Erreur lors de la récupération du Pokémon avec l'ID : {pokemon_id}")
+            return None
 
-def get_random_pokemons():
-    """
-    Fetches a list of random pokemons.
+    @staticmethod
+    def get_random_pokemons():
+        """
+        Fetches a list of random Pokémon data.
+        """
+        pokemons = []
+        response = requests.get(URL_POKEMON_API_BASE)
+        total_pokemons = response.json()["count"]
 
-    :return: A list of dictionaries, where each dictionary contains data about a random pokemon.
-    """
-    pokemons = []
-    response = requests.get(URL_POKEMON_API_BASE)
-    pokemons_count = response.json()["count"]
+        while len(pokemons) < PARTICIPANTS_COUNT:
+            pokemon_id = PokemonManager.get_random_pokemon_id(total_pokemons)
+            pokemon_data = PokemonManager.fetch_pokemon_data(pokemon_id)
+            if pokemon_data and pokemon_data not in pokemons:
+                pokemons.append(pokemon_data)
 
-    while len(pokemons) < NB_PARTICIPANTS:
-        pokemon_id = get_random_pokemon_id(pokemons_count)
-        pokemon_data = fetch_pokemon_data(pokemon_id)
-        if pokemon_data and pokemon_data not in pokemons:
-            pokemons.append(pokemon_data)
-    return pokemons
+        return pokemons
 
-def calculate_pokemon_strength(pokemon):
-    """
-    :param pokemon: A dictionary containing information about a Pokémon, including its stats
-    :type pokemon: dict
-    :return: The total strength of the Pokémon calculated by summing its base stats
-    :rtype: int
-    """
-    stats = pokemon['stats']
-    total_strength = sum(stat['base_stat'] for stat in stats)
-    return total_strength
+    @staticmethod
+    def calculate_pokemon_strength(pokemon):
+        """
+        Calculate total strength of a Pokémon.
+        """
+        return sum(stat['base_stat'] for stat in pokemon['stats'])
 
-def simulate_battle(pokemon1, pokemon2):
-    """
-    :param pokemon1: The first Pokemon involved in the battle.
-    :param pokemon2: The second Pokemon involved in the battle.
-    :return: The Pokemon that wins the battle, chosen randomly in case of a tie.
-    """
-    strength1 = calculate_pokemon_strength(pokemon1)
-    strength2 = calculate_pokemon_strength(pokemon2)
+    @staticmethod
+    def simulate_battle(pokemon1, pokemon2):
+        """
+        Simulate a battle between two Pokémon.
+        """
+        strength1 = PokemonManager.calculate_pokemon_strength(pokemon1)
+        strength2 = PokemonManager.calculate_pokemon_strength(pokemon2)
 
-    if strength1 > strength2:
-        return pokemon1
-    elif strength2 > strength1:
-        return pokemon2
-    else:
-        return random.choice([pokemon1, pokemon2])
+        return random.choice([pokemon1, pokemon2]) if strength1 == strength2 else (
+            pokemon1 if strength1 > strength2 else pokemon2)
+
 
 @app.route('/')
 def index():
     """
     Handles the root URL of the application.
-
-    This route is responsible for initializing a global list of participants
-    by fetching random Pokémon data. It then renders the `index.html` template,
-    passing in the number of participants and the list of Pokémon.
-
-    :return: Rendered HTML for the index page.
     """
     global participants
-    participants = get_random_pokemons()
-    return render_template('index.html', nbParticipants=NB_PARTICIPANTS, pokemons=participants)
+
+    participants = PokemonManager.get_random_pokemons()
+    return render_template('index.html', nbParticipants=PARTICIPANTS_COUNT, pokemons=participants)
+
 
 @app.route('/tournament')
 def tournament():
     """
-    Handles the tournament route, simulating a series of battles between Pokémon participants until a champion is determined.
-
-    :return: Renders the tournament results page with details of each round and the final champion.
+    Simulates a series of battles until a Pokémon champion is determined.
     """
     global participants
-    pokemons = participants
+
     rounds = []
     round_number = 1
 
-    while len(pokemons) > 1:
+    while len(participants) > 1:
         round_battles = []
         winners = []
 
-        for i in range(0, len(pokemons), 2):
-            pokemon1 = pokemons[i]
-            pokemon2 = pokemons[i + 1]
-            winner = simulate_battle(pokemon1, pokemon2)
+        for i in range(0, len(participants), 2):
+            pokemon1 = participants[i]
+            pokemon2 = participants[i + 1]
+            winner = PokemonManager.simulate_battle(pokemon1, pokemon2)
             winners.append(winner)
-
-            # Ajout des détails du combat (Pokémon 1, Pokémon 2, Vainqueur)
             round_battles.append({
                 'pokemon1': pokemon1['name'],
                 'pokemon1_img': pokemon1['sprites']['front_default'],
@@ -126,13 +115,12 @@ def tournament():
             'battles': round_battles
         })
 
-        pokemons = winners
+        participants = winners
         round_number += 1
 
-    # Le dernier Pokémon restant est le champion
-    champion = pokemons[0]
-
+    champion = participants[0]
     return render_template('tournament.html', rounds=rounds, champion=champion)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
