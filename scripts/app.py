ddbaf92f0394d5257cd8b691aaa760c19a30859e
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-08 11:36:03 +0200

    Refactor constants and improve function clarity.
    Refactored URL and participant count constants for improved readability. Updated function parameter names and comments to enhance code clarity and consistency. Removed redundant comments and optimized control flow in various functions for better maintainability.

--- scripts/app.py
+++ scripts/app.py
@@ -4,96 +4,94 @@ import requests
 
 app = Flask(__name__)
 
-BASE_URL_API = "https://pokeapi.co/api/v2/"
-URL_POKEMON_API_BASE = "%spokemon" % BASE_URL_API
-NB_PARTICIPANTS = 16
+# Constants
+BASE_API_URL = "https://pokeapi.co/api/v2/"
+POKEMON_API_URL = f"{BASE_API_URL}pokemon"
+NUM_PARTICIPANTS = 16
 
 selected_pokemons = []
 
-def get_random_pokemon_id(pokemons_count):
+
+def get_random_pokemon_id(total_pokemons):
     """
-    :param pokemons_count: Total number of available Pokémon
-    :return: A random Pokémon ID between 1 and pokemons_count (inclusive)
+    :param total_pokemons: The total number of Pokémon available.
+    :return: A random Pokémon ID between 1 and the total number of Pokémon.
     """
-    return random.randint(1, pokemons_count)
+    return random.randint(1, total_pokemons)
+
 
 def fetch_pokemon_data(pokemon_id):
     """
-    :param pokemon_id: The ID of the Pokémon to fetch data for.
+    :param pokemon_id: The ID of the Pokémon to retrieve data for.
     :return: A dictionary containing Pokémon data if the request is successful, None otherwise.
     """
+    url = f"{POKEMON_API_URL}/{pokemon_id}"
     try:
-        url_pokemon = f"{URL_POKEMON_API_BASE}/{pokemon_id}"
-        response = requests.get(url_pokemon)
+        response = requests.get(url)
         response.raise_for_status()
         return response.json()
     except requests.RequestException:
-        print(f"Erreur lors de la récupération du Pokémon avec l'ID : {pokemon_id}")
+        print(f"Error retrieving Pokémon with ID : {pokemon_id}")
         return None
+
 
 def get_random_pokemons():
     """
     Fetch a list of random Pokémon data.
 
-    :return: A list of dictionaries, each containing data for a random Pokémon
+    :return: A list of dictionaries, each containing data for a unique Pokémon.
     """
     pokemons = []
-    response = requests.get(URL_POKEMON_API_BASE)
-    pokemons_count = response.json()["count"]
-
-    while len(pokemons) < NB_PARTICIPANTS:
-        pokemon_id = get_random_pokemon_id(pokemons_count)
+    response = requests.get(POKEMON_API_URL)
+    total_pokemons = response.json()["count"]
+    while len(pokemons) < NUM_PARTICIPANTS:
+        pokemon_id = get_random_pokemon_id(total_pokemons)
         pokemon_data = fetch_pokemon_data(pokemon_id)
         if pokemon_data and pokemon_data not in pokemons:
             pokemons.append(pokemon_data)
     return pokemons
 
+
 def calculate_pokemon_strength(pokemon):
     """
-    :param pokemon: A dictionary representing a Pokémon, which includes its stats.
-    :type pokemon: dict
-    :return: The total strength value calculated by summing base stats of the Pokémon.
-    :rtype: int
+    :param pokemon: A dictionary representing a Pokémon, containing various stats.
+    :return: The total strength of the Pokémon calculated by summing up its base stats.
     """
-    stats = pokemon['stats']
-    total_strength = sum(stat['base_stat'] for stat in stats)
-    return total_strength
+    return sum(stat['base_stat'] for stat in pokemon['stats'])
+
 
 def simulate_battle(pokemon1, pokemon2):
     """
     :param pokemon1: The first Pokémon participating in the battle.
     :param pokemon2: The second Pokémon participating in the battle.
-    :return: The Pokémon that wins the simulated battle.
+    :return: The winning Pokémon after the simulation.
     """
     strength1 = calculate_pokemon_strength(pokemon1)
     strength2 = calculate_pokemon_strength(pokemon2)
-
     if strength1 > strength2:
         return pokemon1
-    elif strength2 > strength1:
+    if strength2 > strength1:
         return pokemon2
-    else:
-        return random.choice([pokemon1, pokemon2])
+    return random.choice([pokemon1, pokemon2])
+
 
 def simulate_tournament(pokemons):
     """
-    :param pokemons: A list of dictionaries where each dictionary contains information about a Pokémon, including its name and sprites.
-    :return: A tuple containing a list of rounds with details of each battle, and the final champion Pokémon.
+    :param pokemons: A list of dictionaries, each representing a Pokémon with its attributes such as name and sprites.
+    :return: A tuple containing:
+        - rounds: A list of dictionaries, each representing a round with its battles.
+        - The final winning Pokémon as a dictionary.
     """
     rounds = []
-    round_number = 1
-
+    round_num = 1
     while len(pokemons) > 1:
         round_battles = []
         winners = []
-
         for i in range(0, len(pokemons), 2):
             pokemon1 = pokemons[i]
             pokemon2 = pokemons[i + 1]
             winner = simulate_battle(pokemon1, pokemon2)
             winners.append(winner)
-
-            # Ajout des détails du combat (Pokémon 1, Pokémon 2, Vainqueur)
             round_battles.append({
                 'pokemon1': pokemon1['name'],
                 'pokemon1_img': pokemon1['sprites']['front_default'],
@@ -101,46 +99,45 @@ def simulate_tournament(pokemons):
                 'pokemon2_img': pokemon2['sprites']['front_default'],
                 'winner': winner['name']
             })
-
         rounds.append({
-            'round_number': round_number,
+            'round_number': round_num,
             'battles': round_battles
         })
-
         pokemons = winners
-        round_number += 1
+        round_num += 1
+    return rounds, pokemons[0]
 
-    return rounds, pokemons[0]  # Retourne les rounds et le champion
 
 @app.route('/')
 def index():
     """
-    Handles the root URL of the application.
+    Route for the home page.
 
-    :return: Rendered HTML page with a list of randomly selected Pokémon.
+    This function handles the root URL endpoint ('/') and performs the following actions:
+    1. Sets the `selected_pokemons` global variable with a list of random Pokemon objects by calling the `get_random_pokemons` function.
+    2. Renders the 'participants.html' template, passing the list of selected Pokemon objects to it.
+
+    :return: Rendered HTML page for the participants.html template with the selected Pokemon
     """
     global selected_pokemons
-    selected_pokemons = get_random_pokemons()  # Obtenir et stocker les Pokémon aléatoires
+    selected_pokemons = get_random_pokemons()
     return render_template('participants.html', pokemons=selected_pokemons)
+
 
 @app.route('/tournament/<int:round_number>')
 def tournament(round_number):
     """
-    :param round_number: The current round number to display in the tournament.
-    :return: Rendered HTML template for either the current round of the tournament or the champion if the tournament is complete.
+    :param round_number: The current round number of the tournament being accessed.
+    :return: A rendered HTML template for the specified tournament round. If the round exceeds the total number of rounds, returns the champion page.
     """
     global selected_pokemons
-    rounds, champion = simulate_tournament(selected_pokemons.copy())  # On passe une copie des pokémons
-
-    # Si le numéro du tour est supérieur au nombre de tours, afficher le champion
+    rounds, champion = simulate_tournament(selected_pokemons.copy())
     if round_number > len(rounds):
         return render_template('champion.html', champion=champion)
-
-    # Navigation : obtenir le tour en cours et les données nécessaires
-    current_round = rounds[round_number - 1]  # Les tours commencent à 1
+    current_round = rounds[round_number - 1]
     total_rounds = len(rounds)
-
     return render_template('tournament.html', round=current_round, round_number=round_number, total_rounds=total_rounds)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
